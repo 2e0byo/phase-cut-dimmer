aabff3b73901892be706a43342c9e8a15b4d013f
John Maximilian <2e0byo@gmail.com>
2022-02-23 11:49:24 +0000

    feat: dimmer set duty.

--- dimmer.c
+++ dimmer.c
@@ -4,13 +4,18 @@
 
 static unsigned int period = 0;
 
+unsigned int duty = 0;
+
 static unsigned int calcPeriod(unsigned int period) {
   return 0xffff - ((_XTAL_FREQ / 4000000) * period);
 }
 
-void setDuty(unsigned int duty) {
-  period = duty? calcPeriod((unsigned long)10000 * duty / 0x3ff) : 0;
+void setDuty(unsigned int val) {
+  val %= 1023;
+  period = val? calcPeriod((unsigned long)10000 * val / 0x3ff) : 0;
+  duty = val;
 }
+
 
 void dimmerISR(void) {
   if (PIR1bits.TMR1IF == 1) {
