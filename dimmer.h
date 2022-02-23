19f14c1496f05f5e2917494cfc81209e0f603da1
John Maximilian <2e0byo@gmail.com>
2022-02-23 11:50:06 +0000

    chore: header file for dimmer.

--- dimmer.h
+++ dimmer.h
@@ -1,3 +1,18 @@
+/**
+ * @brief Set duty.
+ * @details Use this fn to set duty: it calculates things.
+ * @param[in] duty Duty to set (max 1023).
+ */
 void setDuty(unsigned int duty);
 
+/**
+ * @var duty
+ * @brief The current duty (max 1023).
+ */
+unsigned int duty;
+
+/**
+ * @brief ISR for the dimmer.
+ * @details Call this fn from your global ISR.
+ */
 void dimmerISR(void);
